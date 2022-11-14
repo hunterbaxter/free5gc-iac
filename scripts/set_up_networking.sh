6b67a20861240365d4637ecc561b61c8496029c6
Hunter Baxter <hunter.c.baxter@vanderbilt.edu>
2022-11-13 20:39:25 -0600

    notes and changes for v3.2.1

--- scripts/set_up_networking.sh
+++ scripts/set_up_networking.sh
@@ -1,3 +1,7 @@
-sudo sysctl -w net.ipv4.ip_forward=1;
-sudo iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE;
-sudo systemctl stop ufw;
+# sudo sysctl -w net.ipv4.ip_forward=1;
+# sudo iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE;
+# sudo systemctl stop ufw;
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
+sudo iptables -A FORWARD -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1400
+sudo systemctl stop ufw
